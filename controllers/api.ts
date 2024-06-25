a2afdd538174a46041d9d01923ec7059c6109c08
dani mnh <dani.n@spacetoongo.com>
2024-06-25 21:59:19 +0700

    feat: apiError & param checks

--- controllers/api.ts
+++ controllers/api.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import firebase from '../config/firebaseConfig';
+import ApiError from '../utils/error/apiError';
 
 export const addUserData = async (
   req: Request,
@@ -8,6 +9,10 @@ export const addUserData = async (
 ) => {
   try {
     const { userId, data } = req.body;
+    // handle error params
+    if (!userId || !data) {
+      throw ApiError.badRequest('UserId and data are required');
+    }
     await firebase.db.collection('USERS').doc(userId).set(data);
     res.status(200).json({ message: 'User data added successfully' });
   } catch (error) {
@@ -22,6 +27,9 @@ export const updateUserData = async (
 ) => {
   try {
     const { userId, data } = req.body;
+    if (!userId || !data) {
+      throw ApiError.badRequest('UserId and data are required');
+    }
     await firebase.db
       .collection('USERS')
       .doc(userId)
@@ -39,6 +47,9 @@ export const fetchUserData = async (
 ) => {
   try {
     const { userId } = req.params;
+    if (!userId) {
+      throw ApiError.badRequest('UserId are required');
+    }
     const userDoc = await firebase.db.collection('USERS').doc(userId).get();
     res.status(200).json(userDoc.data());
   } catch (error) {
