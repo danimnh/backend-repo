44dc1c1598cdefa8571ece23ba04de277aa4e167
dani mnh <dani.n@spacetoongo.com>
2024-06-27 01:35:59 +0700

    feat: add cors()

--- index.ts
+++ index.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 import userRoutes from './routes/userRoute';
+import cors from 'cors';
 
 const app = express();
 const port = 3000;
-
+app.use(cors());
 app.use(express.json());
 
 app.use('/api', userRoutes);
